069b52dc83ca98838fceb1de726898ba491b0258
8em <jpeg.oleg@gmail.com>
2024-10-31 02:45:10 +0200

    font added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Marquee from 'react-fast-marquee'
 import PrimaryButton from '@/components/PrimaryButton'
 import SecondaryButton from '@/components/SecondaryButton'
 
+
 export default function Home() {
   return (
     <div>
--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,21 +1,21 @@
-import { Button as HeadlessButton } from '@headlessui/react'
-import { cn } from '@/lib/utils'
+import { Button as HeadlessButton } from "@headlessui/react";
+import { cn } from "@/lib/utils";
 
 const getButtonClasses = ({
   disabled,
   active,
 }: {
-  disabled?: boolean
-  active?: boolean
+  disabled?: boolean;
+  active?: boolean;
 }) =>
   cn(
-    'w-full lg:w-60 py-3 rounded-3xl transition-colors text-zinc-50',
+    "w-full lg:w-60 py-3 rounded-3xl transition-colors text-zinc-50",
     disabled
-      ? 'bg-gray-400 cursor-not-allowed'
-      : (active ? 'bg-blue-700' : 'bg-blue-500') +
-          ' ' +
-          (!disabled ? 'hover:bg-blue-600' : '')
-  )
+      ? "bg-gray-400 cursor-not-allowed"
+      : (active ? "bg-blue-700" : "bg-blue-500") +
+          " " +
+          (!disabled ? "hover:bg-blue-600" : "")
+  );
 
 const PrimaryButton = ({
   children,
@@ -23,18 +23,18 @@ const PrimaryButton = ({
   active = false,
   ...props
 }: {
-  children: React.ReactNode
-  disabled?: boolean
-  active?: boolean
-  [x: string]: any
+  children: React.ReactNode;
+  disabled?: boolean;
+  active?: boolean;
+  [x: string]: any;
 }) => (
   <HeadlessButton
-    className={getButtonClasses({ disabled, active })}
+    className={`${getButtonClasses({ disabled, active })} text-sm`}
     disabled={disabled}
     {...props}
   >
     {children}
   </HeadlessButton>
-)
+);
 
-export default PrimaryButton
+export default PrimaryButton;
